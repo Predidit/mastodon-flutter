a50e4da46d551ac692251e81f96f5bd00d906577
Predidit <songjianwork@gamil.com>
2024-02-23 20:51:08 +0800

    bug fix

--- lib/core/mastodon.dart
+++ lib/core/mastodon.dart
@@ -1,21 +1,28 @@
 import 'package:mastodon_api/mastodon_api.dart';
 import 'package:mastodon_flutter/source/api_config.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class Mastodon {
-  final mastodon = MastodonApi(
-    instance: 'MASTODON_INSTANCE',
-    bearerToken: 'YOUR_BEARER_TOKEN',
-    retryConfig: RetryConfig(
-      maxAttempts: 5,
-      jitter: Jitter(
-        minInSeconds: 2,
-        maxInSeconds: 5,
+  late APIConfig apiConfig;
+  late MastodonApi mastodon;
+
+  Mastodon() {
+    APIConfig apiConfig = Modular.get<APIConfig>();
+    mastodon = MastodonApi(
+      instance: apiConfig.mastondonInstance,
+      bearerToken: apiConfig.breaveToken,
+      retryConfig: RetryConfig(
+        maxAttempts: 5,
+        jitter: Jitter(
+          minInSeconds: 2,
+          maxInSeconds: 5,
+        ),
+        onExecute: (event) => print(
+          'Retry after ${event.intervalInSeconds} seconds... '
+          '[${event.retryCount} times]',
+        ),
       ),
-      onExecute: (event) => print(
-        'Retry after ${event.intervalInSeconds} seconds... '
-        '[${event.retryCount} times]',
-      ),
-    ),
-    timeout: const Duration(seconds:10),
-  );
+      timeout: const Duration(seconds: 10),
+    );
+  }
 }
